d36300d5706263586bff897bdc70eb6ee7399efe
Igor Gerbylev <gerbylev@gmail.com>
2020-11-07 11:47:02 +0300

    rewrite api spec

---
+++ server/src/main/kotlin/chatcontact/endpoints/AdminDataApiImpl.kt
@@ -0,0 +1,37 @@
+package chatcontact.endpoints
+
+import chatcontact.api.AdminDataApiService
+import chatcontact.api.model.CandidateData
+import chatcontact.api.model.MatchRequestData
+import chatcontact.api.model.StatData
+import chatcontact.api.model.UserData
+import chatcontact.services.DataService
+import org.springframework.stereotype.Service
+
+@Service
+class AdminDataApiImpl(val data: DataService) : AdminDataApiService {
+    override fun createUser(userData: UserData): UserData {
+        TODO("Not yet implemented")
+    }
+
+    override fun getCounters(): StatData {
+        return data.getCounters()
+    }
+
+    override fun getMatching(userId: Long): MatchRequestData {
+        TODO("Not yet implemented")
+    }
+
+    override fun getMatchingCandidates(userId: Long, matchRequestId: Long): List<CandidateData> {
+        TODO("Not yet implemented")
+    }
+
+    override fun listUsers(): List<UserData> {
+        return data.listUsers()
+    }
+
+    override fun startMatching(userId: Long, matchRequestData: MatchRequestData): MatchRequestData {
+        TODO("Not yet implemented")
+    }
+
+}
